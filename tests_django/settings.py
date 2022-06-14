1757ce9b9f204abf5c5e31d278cb4c00cf8cef36
Quentin BEY <quentin.bey@polyconseil.fr>
2022-06-14 17:31:04 +0200

    👷(django) init tests for Django integration
    This add Django tests in the tox testing suite.

---
+++ tests_django/settings.py
@@ -0,0 +1,46 @@
+"""Django test project settings"""
+import os
+
+from django.core.management.utils import get_random_secret_key
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DEBUG = True
+USE_TZ = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
+ROOT_URLCONF = "tests_django.urls"
+
+INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "django.contrib.admin",
+    "social_django",
+]
+
+SITE_ID = 1
+
+MIDDLEWARE = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'social_django.middleware.SocialAuthExceptionMiddleware',
+)
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+SECRET_KEY = get_random_secret_key()
+
+STATIC_URL = '/static/'
